14faa67304f971cdde003ce980ed81ad6e3d9b0c
RAMEEN NAEEM <154015179+rameen22@users.noreply.github.com>
2024-01-23 17:35:15 +0500

    Update server.js

--- Express APP Setup/server.js
+++ Express APP Setup/server.js
@@ -1,1 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+//express app
+const app = express()
+//next is used to move from one middleware to the next
+//middleware
+app.use((req,res, next) =>{
+console.log(req.path, req.method)
+next()
+})
+//now we want an app to react also
+app.get('/', (req, res) => {
+    res.json({mssg:'Welcome to the App'})
+})//this will response to a get req comming in from a specific route
+//for sensitive info we move them into environmental variable
+//env file to git ignore file
+
+
+
+//to listen for requests(certain port numb)
+app.listen(process.env.PORT, () => {
+    console.log('listenting on port', process.env.PORT)
+})
+//now run the file
+//if we change anything inthe code it will not run we have to rerun the code in node 
+//install nodemon pkg globally => npm install -g nodemon then nodemon filename
+//now if we change anything it detect it and it rerun the file 
 
