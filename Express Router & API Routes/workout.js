6b9e5608ffa3b5ed33a476295fde084bcc25d02c
RAMEEN NAEEM <154015179+rameen22@users.noreply.github.com>
2024-01-23 22:20:06 +0500

    Update workout.js

--- Express Router & API Routes/workout.js
+++ Express Router & API Routes/workout.js
@@ -1,1 +1,31 @@
+const express = require('express')
 
+const router = express.Router()
+
+//to get all workouts
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all workouts'})
+})
+
+//GET single workout
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET single workout'})
+})
+
+//POST a workout
+router.post('/', (req, res) => {
+    res.json({mssg: 'post a workout'})
+})
+//DELETE a workout
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a workout'})
+})
+
+//UPDATE a workout
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'patch a workout'})
+})
+
+
+//exporting routers
+module.exports = router
