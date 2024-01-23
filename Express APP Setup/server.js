bc90e9d372f1eeb2b4b83b3b6ea0df9dcb9e189e
RAMEEN NAEEM <154015179+rameen22@users.noreply.github.com>
2024-01-23 22:20:53 +0500

    Update server.js

--- Express APP Setup/server.js
+++ Express APP Setup/server.js
@@ -1,17 +1,31 @@
 require('dotenv').config()
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+
+
 //express app
 const app = express()
+
+
 //next is used to move from one middleware to the next
 //middleware
+app.use(express.json())
 app.use((req,res, next) =>{
 console.log(req.path, req.method)
 next()
 })
+
+//routes (it grabs all the routes attached in workout file)
+app.use('/api/workouts',workoutRoutes)
+
+
+
+
 //now we want an app to react also
-app.get('/', (req, res) => {
-    res.json({mssg:'Welcome to the App'})
-})//this will response to a get req comming in from a specific route
+//testroutes
+//app.get('/', (req, res) => {
+    //res.json({mssg:'Welcome to the App'})
+//})//this will response to a get req comming in from a specific route
 //for sensitive info we move them into environmental variable
 //env file to git ignore file
 
@@ -25,4 +39,3 @@ app.listen(process.env.PORT, () => {
 //if we change anything inthe code it will not run we have to rerun the code in node 
 //install nodemon pkg globally => npm install -g nodemon then nodemon filename
 //now if we change anything it detect it and it rerun the file 
-
